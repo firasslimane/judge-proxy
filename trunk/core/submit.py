540d3ab96fa5999ad37ce0b628ea1e124fc6917d
IamSempr <IamSempr@4c7018cc-ccdf-11dd-8829-37334faa441c>
2008-12-20 10:26:49 +0000

    get a line, added!

--- trunk/core/submit.py
+++ trunk/core/submit.py
@@ -1,13 +1,25 @@
-import urllib,urllib2,sys
+import urllib,urllib2,sys,string
+
+def UVA_GetFirstLine(url):
+    f=urllib.urlopen(url)
+    lines=f.readlines()
+    f.close()
+    
+    i=0 
+    for  i in range(len(lines)):
+        lines[i]=lines[i].strip('\t\n ')
+    aline=string.join(lines,'')
+    start=aline.find('<tr')
+    start=aline.find('<tr',start+1)
+    end=aline.find('<tr',start+1)
+    print aline[start:end]
 
 def UVA_Submit(problem,userid,language,code):
     url  = "http://acmicpc-live-archive.uva.es/nuevoportal/mailer.php"
+    sturl = "http://acmicpc-live-archive.uva.es/nuevoportal/status.php"
+    UVA_GetFirstLine(sturl)
     data = urllib.urlencode([('problem',problem),('userid',userid),('language',language),('code',code)])
     req = urllib2.Request(url)
     fd = urllib2.urlopen(req,data)
-    while 1:
-        dt = fd.read(1024)
-        if not len(dt):
-            break
     return 0
 
